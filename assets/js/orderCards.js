da553bf54efa069a982673d1d396094f5ad34d84
slashflex <pro.davidsaoud@gmail.com>
2020-06-11 18:37:54 +0200

    method to range cards from high to low

---
+++ assets/js/orderCards.js
@@ -0,0 +1,61 @@
+/*
+ * /!\ À essayer seul, me mp si trop compliqué !
+ * Le but de cette fonction est de réorganiser un tableau de cartes de la meilleur à la moins bonnes :
+ * cards = ['Ad', '7h', '8d', 'As', 'Qs'] alors orderCards(cards) retournera ['As', 'Ad', 'Qs', '8d', '7h']
+ *
+ * Une carte est une chaîne de caractère qui contient deux parties :
+ * - La valeur de la carte (ordre croissant): 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+ * - Le type de la carte (ordre croissant) : d => ♦, c => ♣, h => ♥, s => ♠
+ *
+ * On considère que la valeur de la carte prime sur le type de la carte
+ */
+
+const VALUES_POWER = {
+  A: 14,
+  Q: 13,
+  K: 12,
+  J: 11,
+  '1': 10,
+  '9': 9,
+  '8': 8,
+  '7': 7,
+  '6': 6,
+  '5': 5,
+  '4': 4,
+  '3': 3,
+  '2': 2,
+};
+const TYPES_POWER = {
+  s: 4,
+  h: 3,
+  c: 2,
+  d: 1,
+};
+
+export const orderCards = cards => {
+  return cards
+    .sort((a, b) => {
+      if (valuePower(a) < valuePower(b)) {
+        return -1;
+      } else if (valuePower(a) > valuePower(b)) {
+        return 1;
+      } else {
+        if (typePower(a) < typePower(b)) {
+          return -1;
+        } else if (typePower(a) > typePower(b)) {
+          return 1;
+        }
+      }
+    })
+    .reverse();
+}
+// console.log(orderCards(['10s', 'Qs', 'Qh', 'Qc']));
+
+function valuePower(card) {
+  let key = card.charAt(0);
+  return VALUES_POWER[key];
+}
+function typePower(card) {
+  let key = card.charAt(card.length - 1);
+  return TYPES_POWER[key];
+}
