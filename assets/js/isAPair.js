417ef6a0bace4c5675d2233c872e4962b65326b6
slashflex <pro.davidsaoud@gmail.com>
2020-06-11 18:36:29 +0200

    method to check if there's a pair

---
+++ assets/js/isAPair.js
@@ -0,0 +1,28 @@
+/*
+ * /!\ À faire seul !
+ *
+ * Retourne un boolean qui indique si le tableau cards contient une pair, exemple :
+ * cards = ["As", "10d", "Jd", "Qs", "Ah", "2h", "3c"] isPaired(cards) retournera true
+ * cards = ["As", "10d", "Jd", "Qs", "2h", "3c", "5d"] isPaired(cards) retournera false
+ *
+ * Attention, une carte est une chaîne de caractère qui contient deux parties :
+ * - La valeur de la carte : 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+ * - Le type de la carte : s => ♠, h => ♥, c => ♣, d => ♦
+ */
+
+// Check whether or not there's a pair
+export const isAPair = cards => {
+  for (let i = 0; i < cards.length; i++) {
+    for (let j = 0; j < cards.length; j++) {
+      if (i != j) {
+        if (
+          cards[i].charAt(0) ==
+          cards[j].charAt(0)
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
