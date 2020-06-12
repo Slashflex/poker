0cbe21f5bdda1792d31bc7187900993d4d8cd92d
slashflex <pro.davidsaoud@gmail.com>
2020-06-12 16:44:34 +0200

    feat: is a full function

---
+++ assets/js/isAFull.js
@@ -0,0 +1,32 @@
+/*
+ * /!\ À faire seul !
+ *
+ * Retourne un boolean qui indique si le tableau cards contient une pair, exemple :
+ * cards = ["As", "Ad", "Ac", "Qs", "Qh", "2h", "3c"] isAFull(cards) retournera true
+ * cards = ["As", "10d", "Jd", "Qs", "2h", "3c", "5d"] isAFull(cards) retournera false
+ *
+ * Attention, une carte est une chaîne de caractère qui contient deux parties :
+ * - La valeur de la carte : 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+ * - Le type de la carte : s => ♠, h => ♥, c => ♣, d => ♦
+ */
+
+import { occurencesValue } from './occurences.js';
+
+export const isAFull = (cards) => {
+
+  const occ = occurencesValue(cards);
+  let two = false;
+  let three = false;
+
+  for (const [key, value] of Object.entries(occ)) {
+    
+		if (value == 2) {
+      two = true;
+    }
+    
+    if (value == 3) {
+      three = true;
+		}
+  }
+  return two && three;
+};
